21a1675b8ce77041344afaaa6e29562ef0cecb0a
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-07 18:31:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons_by_name = {}
+
+  data.each { |key, value| 
+    key.each { | nKey, nValue| #nKey is a symbol value, nValue is an array
+      nValue.map { |element| ## arrays of names 
+        if !pigeons_by_name[element]
+          pigeons_by_name[element] = nKey           
+        }
+    }
+  }
 end
